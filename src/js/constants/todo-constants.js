24ce93a7060662f269d58c66ee90ea1e2bfe6ed8
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2014-08-20 01:50:05 +0400

    better constants with keyMirror

--- src/js/constants/todo-constants.js
+++ src/js/constants/todo-constants.js
@@ -1,9 +1,11 @@
-var TodoConstants = {
-  TODO_CREATE: 'todo.create',
-  TODO_DESTROY: 'todo.destroy',
-  TODO_COMPLETE: 'todo.complete',
-  TODO_TOGGLE_COMPLETE_ALL: 'todo.toggleCompleteAll',
-  TODO_UNCOMPLETE: 'todo.uncomplete',
-  TODO_UPDATE_TEXT: 'todo.updateText',
-  TODO_DESTROY_COMPLETED: 'todo.destroyCompleted'
-};
+var keyMirror = require('react/lib/keyMirror');
+
+var TodoConstants = keyMirror({
+  TODO_CREATE: null,
+  TODO_DESTROY: null,
+  TODO_COMPLETE: null,
+  TODO_TOGGLE_COMPLETE_ALL: null,
+  TODO_UNCOMPLETE: null,
+  TODO_UPDATE_TEXT: null,
+  TODO_DESTROY_COMPLETED: null
+});
