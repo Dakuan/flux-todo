040fbcf4133fa0adb672127cf25f31f3f698e277
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2014-08-20 02:34:38 +0400

    add MainSection component

---
+++ src/js/components/MainSection.react.js
@@ -0,0 +1,43 @@
+/*** @jsx React.DOM */
+
+_ = require('lodash');
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var TodoActions = require('../actions/todo-actions.js');
+var TodoItem = require('./TodoItem.react.js');
+
+var MainSection = React.createClass({
+  propTypes: {
+    source: PropTypes.object.isRequired,
+    areAllComplete: PropTypes.bool.isRequired
+  },
+
+  render: function() {
+    var source = this.props.source;
+
+    if (_.size(source) < 1) {
+      return null;
+    }
+
+    var todos = [];
+    _.forIn(source, function(value, key) {
+      todos.push(<TodoItem key={key} todo={source[key]} />);
+    });
+
+    return (
+      <section id='main'>
+        <input
+          id='toggle-all'
+          type='checkbox'
+          onChange={this._onToggleCompleteAll}
+          checked={this.props.areAllComplete ? 'checked' : '' }
+        />
+        <label htmlFor='toggle-all'>Mark all as complete</label>
+        <ul id='todo-list'>{todos}</ul>
+      </section>
+    );
+  }
+});
+
+module.exports = MainSection;
